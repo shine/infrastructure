2a753233c7168b93ed464a46362952653eb90f70
Victor Brylew <victor.brylew@gmail.com>
2016-03-12 22:20:23 +0100

    Update stop.rb

--- stop.rb
+++ stop.rb
@@ -1,3 +1,8 @@
 # todo
 # this script should do the opposite of spawn.rb
 # it should stop the instance tagged samwecom
+
+ec2 = Aws::EC2::Resource.new(region: 'us-east-1')
+
+running_instance = ec2.instances.detect{|i| i.tags["role"] && i.tags["role"] == "samwecom"} # role name samwecom should be moved to configuration file when we will have config here
+ec2.client.stop_instances({instance_ids: [running_instance.instance_id]})
